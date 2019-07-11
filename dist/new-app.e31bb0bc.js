a9edebb3171dd3353c6c07cf2cb634c537fa9c0a
chris.f.hull@gmail.com <chris.f.hull@gmail.com>
2019-07-10 20:48:27 -0400

    updates data

--- dist/new-app.e31bb0bc.js
+++ dist/new-app.e31bb0bc.js
@@ -28738,7 +28738,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40315" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36807" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
